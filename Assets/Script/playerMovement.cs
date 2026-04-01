050eebb9c1d02c1fcb1fd0c83530d10a57a9a01b
HariniSolai <harisolaid05@gmail.com>
2026-04-01 00:00:31 -0500

    Added forcefield for cave limit

--- Assets/Script/playerMovement.cs
+++ Assets/Script/playerMovement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-    public float speed = 3.0f;
+    public float speed = 20.0f;
     public float rotationSpeed = 45;
     public float jumpForce = 700f;
 
